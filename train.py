13e3fd9253e2b3898a5adbfdb815c17463508de1
buibaogianguyen <buibaogianguyen2008@gmail.com>
2025-08-27 13:13:59 -0400

    train.py

--- train.py
+++ train.py
@@ -1,6 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from model.dataloader import HAM10000
+from data.data_manager import load_metadata, init_db
+
 
 def train_model(model, train_loader, device, epochs=10, lr=0.001):
     criterion = nn.CrossEntropyLoss
@@ -8,4 +13,34 @@ def train_model(model, train_loader, device, epochs=10, lr=0.001):
     
     model.to(device)
 
+    for epoch in range(epochs):
+        model.train()
+
     
+if __name__ == '__main__':
+    transform = transforms.Compose([
+        transforms.Resize(224,224),
+        transforms.RandomHorizontalFlip(p=0.3),
+        transforms.RandomRotation(10),
+        transforms.ColorJitter(0.1, 0.1, 0.1, 0.02),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    batch_size = 32
+
+    session = init_db()
+
+    dataset_path = load_metadata(session)
+    dataset = HAM10000(dataset_path=dataset_path, transform=transform)
+
+    train_size = int(0.8 * len(dataset))
+    val_size = len(dataset) - train_size
+
+    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
+
+    train_loader = DataLoader(train_dataset, batch_size, shuffle=True, num_workers=2)
+    val_loader = DataLoader(val_dataset, batch_size, num_workers=2)
+
+
+
